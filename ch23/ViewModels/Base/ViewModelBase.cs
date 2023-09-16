7a2fb3c6bbd758f7272ff86c150d8cad2171b038
pwshady <pwshady@gmail.com>
2023-09-17 01:57:49 +0300

    interface Disposed

---
+++ ch23/ViewModels/Base/ViewModelBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch23.ViewModels.Base
+{
+    internal abstract class ViewModelBase : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected virtual void OnProprtyCanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value [CallerMemberName] string PropertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnProprtyCanged(PropertyName); 
+            return true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private bool _Disposed;
+
+        protected virtual void Dispose(bool Disposing) 
+        {
+            if (!Disposing || _Disposed) return;
+            _Disposed = true;
+        }
+    }
+}
